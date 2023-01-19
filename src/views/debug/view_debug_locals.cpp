c7ab0109cb2d80392194c06ae6a56780900430ee
TheGreenBandit <106003542+TheGreenBandit@users.noreply.github.com>
2023-01-18 22:30:20 -0500

    removed cutscene stuff
    removed cutscene shit cause i dont wanna work on it rn

--- src/views/debug/view_debug_locals.cpp
+++ src/views/debug/view_debug_locals.cpp
@@ -8,9 +8,6 @@ namespace big
 	{
 		if (ImGui::BeginTabItem("Locals"))
 		{
-			const char cutscene_name = "";
-			const char cutscene_nameflags = "";
-			int flags = 0;
 			if (components::button("Load MP Map"))
 				DLC::ON_ENTER_MP();
 			ImGui::SameLine();
@@ -18,30 +15,11 @@ namespace big
 				DLC::ON_ENTER_SP();
 			if (components::button("Skip Cutscene"))
 				CUTSCENE::STOP_CUTSCENE_IMMEDIATELY();
-			if (components::button("Stop Player Switch"))
-				STREAMING::STOP_PLAYER_SWITCH();
-			if (components::button("Stop Loading"))
+			if (components::button("Show Interior"))
 			{
-				if (CAM::IS_SCREEN_FADED_OUT())
-					CAM::DO_SCREEN_FADE_IN(0);
-				SCRIPT::SHUTDOWN_LOADING_SCREEN();
+				Interior interior = INTERIOR::GET_INTERIOR_AT_COORDS(self::pos.x, self::pos.y, self::pos.z);
+				INTERIOR::REFRESH_INTERIOR(interior);
 			}
-			ImGui::Text("Request Cutscene Types");
-			components::input_text_with_hint("##Name Of Cutscene No Flags", "Simply loads the cutscene and doesn't do extra stuff that REQUEST_CUTSCENE does.", &cutscene_name);
-			ImGui::SameLine();
-			if (components::button("Request"))
-			{
-				CUTSCENE::REQUEST_CUT_FILE(cutscene_name);
-			}
-			components::input_text_with_hint("##With Flag", "flags are usually 8", &cutscene_nameflags);
-			ImGui::InputInt("##Flag", &flags);
-			if (components::button("Request"))
-			{
-				CUTSCENE::REQUEST_CUTSCENE(cutscene_nameflags, flags);
-			}
-
-			REQUEST_CUTSCENE_WITH_PLAYBACK_LIST
-			if hovered Example : 0x105 (bit 0, 2 and 8 set) will enable scene 1, 3 and 9.
 			ImGui::EndTabItem();
 		}
 	}
