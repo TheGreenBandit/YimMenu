7b26a9881c0671deac2a1c7c16b7dbcead3b44c2
TheGreenBandit <106003542+TheGreenBandit@users.noreply.github.com>
2023-02-07 00:21:44 -0500

    unfinished read desc
    need to make it grab random hash from list,
    need to make it random in spawning, from values of 10, -10 on x,y

---
+++ src/backend/looped/player/drops.cpp
@@ -0,0 +1,31 @@
+#include "natives.hpp"
+#include "backend/looped_command.hpp"
+#include "script.cpp"
+
+namespace big
+{
+
+	class beast_jump_looped : looped_command
+	{
+		using looped_command::looped_command;
+
+		virtual void on_tick() override //1666779307//moneybag
+		{
+
+			//0x4D6514A3/*vw_prop_vw_colle_alien*/, 0x748F3A2A/*vw_prop_vw_colle_beast*/, 0x1A9736DA/*vw_prop_vw_colle_imporage*/,0x3D1B7A2F/*vw_prop_vw_colle_pogo*/,
+			//0x1A126315/*vw_prop_vw_colle_prbubble*/, 0xD937A5E9/*vw_prop_vw_colle_rsrcomm*/, 0x23DDE6DB/*vw_prop_vw_colle_rsrgeneric*/, 0x991F8C36/*vw_prop_vw_colle_sasquatch*/
+			Player splayer = PLAYER::GET_PLAYER_PED_SCRIPT_INDEX(PLAYER::GET_PLAYER_PED_SCRIPT_INDEX(g_player_service->get_selected()->id()));
+			Vector3 c = PED::GET_PED_BONE_COORDS(splayer, 46523, 0, 0, 0);
+
+			STREAMING::REQUEST_MODEL(0x4D6514A3);
+			if (STREAMING::HAS_MODEL_LOADED(0x4D6514A3))
+			{
+				OBJECT::CREATE_AMBIENT_PICKUP(0x2C014CA6, c.x, c.y, c.z, NULL, 10, 0x4D6514A3, false, false); 
+				script::get_current()->yield(800ms);
+				STREAMING::SET_MODEL_AS_NO_LONGER_NEEDED(0x4D6514A3);
+			}
+		}
+	};
+
+	beast_jump_looped g_adrop("adrop", "Action Figure Drop", "Allows You To Drop Action Figures On Someone", g.player.action_figure_drop);
+}
